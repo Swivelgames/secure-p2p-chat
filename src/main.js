da4f734459605777bcaca15c02ae5636f21dfb4f
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2016-06-07 08:32:02 -0500

    Joseph - Added prompt-driven encryption/decryption

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 import fs from 'fs';
 import ursa from 'ursa';
+import prompt from 'prompt';
 
 var cert = ursa.createPublicKey(fs.readFileSync('./certs/rsa.pub'));
 var key = ursa.createPrivateKey(fs.readFileSync('./certs/rsa.pem'));
 
-var text = "Hello World!";
-var enc = cert.encrypt(text, 'utf8', 'base64');
-var dec = key.decrypt(enc, 'base64', 'utf8');
+prompt.message = "<you>";
+prompt.delimiter = "";
 
-console.log(dec);
+(function startPrompt(){
+	prompt.start();
+	prompt.get([':'], function(err, result){
+		if(err || !result || !result.hasOwnProperty(':')) return;
+
+		var text = result[':']; //"Hello World!";
+		var enc = cert.encrypt(text, 'utf8', 'base64');
+		var dec = key.decrypt(enc, 'base64', 'utf8');
+
+		console.log(dec);
+		startPrompt();
+	});
+})();
