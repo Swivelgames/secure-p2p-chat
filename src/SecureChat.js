8ac0fbf6929f7c372e3e8acb27c8a36853bf1ee8
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2016-06-07 11:04:41 -0500

    Joseph - Better results switching from `prompt` to just using `stdin/stdout` and `readline`

--- src/SecureChat.js
+++ src/SecureChat.js
@@ -2,6 +2,14 @@ import fs from 'fs';
 import ursa from 'ursa';
 import prompt from 'prompt';
 import yargs from 'yargs';
+import readline from 'readline';
+
+import util from 'util';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 export default class SecureChat {
 	constructor() {
@@ -27,6 +35,23 @@ export default class SecureChat {
 			}
 		};
 
+		this.curInput = "";
+		process.stdin.setEncoding("utf8");
+		process.stdin.on('data', (data) => {
+			this.curInput += data.toString('utf8');
+		});
+
+		rl.on('line', (text) => {
+			this.curInput = "";
+			var localRSA = this.rsa.local;
+			var enc = localRSA.cert.encrypt(text, 'utf8', 'base64');
+			var dec = localRSA.key.decrypt(enc, 'base64', 'utf8');
+
+			console.log(`<remote> ${dec}`);
+
+			this.requestMessage();
+		});
+
 		this.requestMessage();
 	}
 
@@ -34,63 +59,23 @@ export default class SecureChat {
 
 	}
 
+	handleMessageReceived(msg) {
+		process.stdout.clearLine();
+		process.stdout.cursorTo(0);
+		rl.pause();
+		console.log("<remote> Hey, how's it going?");
+		rl.resume();
+		let text = "<you> "+this.curInput;
+		process.stdout.clearLine();
+		process.stdout.cursorTo(0);
+		process.stdout.write(text);
+		process.stdout.cursorTo(text.length);
+	}
+
 	requestMessage() {
-		prompt.message = "<you>";
-		prompt.delimiter = "";
+		rl.setPrompt("<you> ");
+		rl.prompt();
 
-		prompt.get([':'], (err, result) => {
-			if(err || !result || !result.hasOwnProperty(':')) return;
-
-			var localRSA = this.rsa.local;
-			var text = result[':']; //"Hello World!";
-			var enc = localRSA.cert.encrypt(text, 'utf8', 'base64');
-			var dec = localRSA.key.decrypt(enc, 'base64', 'utf8');
-
-			console.log('<remote>: '+dec);
-			this.requestMessage();
-		});
-
-		prompt.start();
-
-		setTimeout( () => {
-			process.stdout.clearLine();
-			process.stdout.cursorTo(0);
-			prompt.pause();
-			console.log("<remote> Hey, how's it going?");
-			prompt.resume();
-		}, 1000 );
+		setTimeout( () => this.handleMessageReceived() , 1000 );
 	}
 }
-
-/*
-
-var argv = yargs.argv, key, cert;
-
-if(argv.t || argv.temp) {
-	console.log("Using a temporary RSA keypair");
-	key = ursa.generatePrivateKey();
-	cert = ursa.createPublicKey(key.toPublicPem());
-} else {
-	console.log("Using RSA keypair in 'certs' folder");
-	cert = ursa.createPublicKey(fs.readFileSync('./certs/rsa.pub'));
-	key = ursa.createPrivateKey(fs.readFileSync('./certs/rsa.pem'));
-}
-
-prompt.message = "<you>";
-prompt.delimiter = "";
-
-(function startPrompt(){
-	prompt.start();
-	prompt.get([':'], function(err, result){
-		if(err || !result || !result.hasOwnProperty(':')) return;
-
-		var text = result[':']; //"Hello World!";
-		var enc = cert.encrypt(text, 'utf8', 'base64');
-		var dec = key.decrypt(enc, 'base64', 'utf8');
-
-		console.log('[: '+enc+' :] => [$ '+dec+' $]');
-		startPrompt();
-	});
-})();
-
-*/
