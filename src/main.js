0dbdd631a4817f20d17ccde99b7829e6377cb432
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2016-06-07 08:05:52 -0500

    Joseph - Added URSA for Public/Private Key usage -- Created example in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-let x = "Testing";
+import fs from 'fs';
+import ursa from 'ursa';
 
-console.log(x);
+var cert = ursa.createPublicKey(fs.readFileSync('./certs/rsa.pub'));
+var key = ursa.createPrivateKey(fs.readFileSync('./certs/rsa.pem'));
+
+var text = "Hello World!";
+var enc = cert.encrypt(text, 'utf8', 'base64');
+var dec = key.decrypt(enc, 'base64', 'utf8');
+
+console.log(dec);
