dcc7d2257b7ebcdc65048bb7a499560e5dc1710f
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2016-06-07 09:41:52 -0500

    Joseph - Moved most functionality into SecureChat.js file -- Began messing around with incoming messages and similar items

--- src/main.js
+++ src/main.js
@@ -1,33 +1,3 @@
-import fs from 'fs';
-import ursa from 'ursa';
-import prompt from 'prompt';
-import yargs from 'yargs';
+import SecureChat from './SecureChat.js';
 
-var argv = yargs.argv, key, cert;
-
-if(argv.t || argv.temp) {
-	console.log("Using a temporary RSA keypair");
-	key = ursa.generatePrivateKey();
-	cert = ursa.createPublicKey(key.toPublicPem());
-} else {
-	console.log("Using RSA keypair in 'certs' folder");
-	cert = ursa.createPublicKey(fs.readFileSync('./certs/rsa.pub'));
-	key = ursa.createPrivateKey(fs.readFileSync('./certs/rsa.pem'));
-}
-
-prompt.message = "<you>";
-prompt.delimiter = "";
-
-(function startPrompt(){
-	prompt.start();
-	prompt.get([':'], function(err, result){
-		if(err || !result || !result.hasOwnProperty(':')) return;
-
-		var text = result[':']; //"Hello World!";
-		var enc = cert.encrypt(text, 'utf8', 'base64');
-		var dec = key.decrypt(enc, 'base64', 'utf8');
-
-		console.log('[: '+enc+' :] => [$ '+dec+' $]');
-		startPrompt();
-	});
-})();
+new SecureChat();
